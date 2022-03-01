e789663e66545befa35e1787ff2ba7f29c86ccc5
Thejaani Udumanne <36429476+tudumanne@users.noreply.github.com>
2022-03-01 14:25:45 +1100

    Update bash-script.sh

--- scripts/bash-script.sh
+++ scripts/bash-script.sh
@@ -3,7 +3,7 @@
 # usage - chip-seq standard pipeline
 
 #change the working directory
-cd ~/chip-seq/experiment_01/miseq_data
+cd ~/chip-seq/experiment_01
 
 #quality check of raw fastq files
 fastqc -o fastqc --extract --dir fastqc_chip --format fastq h3k4me3_*.fastq.gz
@@ -16,27 +16,33 @@ samtools sort {sample}.bam > {sample}_sorted.bam
 samtools index {sample}_sorted.bam
 
 #quality check of bam files
-qualimap bamqc -bam file.bam -outdir qualimap_results -outformat pdf
-multiqc fastqc/
+qualimap bamqc -bam {sample}.bam -outdir qualimap_results -outformat pdf
+multiqc qualimap_results/
 
 #deeptools fingerprint plot
-plotFingerprint -b {sample}_sorted.bam -plot {sample}.pdf --extendReads --centerReads --plotFileFormat pdf --plotTitle "sample"
+plotFingerprint -b {sample}_sorted.bam -plot {sample}.pdf --extendReads --centerReads --plotFileFormat pdf --plotTitle "sample_name"
 
 #deeptools correlation and pca
-multiBamSummary bins --bamfiles *_sorted.bam -o {ChIP target}.npz --binSize 1000 --extendReads --centerReads
-plotCorrelation --corData {ChIP target}.npz --corMethod spearman --whatToPlot heatmap --plotFile correlation_heatmap.pdf --skipZeros --plotFileFormat pdf --plotNumbers --plotTitle "sample"
-plotPCA --corData {ChIP target}.npz --plotFile pca_plot.pdf --plotTitle "sample" --plotFileFormat pdf
+multiBamSummary bins --bamfiles *_sorted.bam -o {ChIP_target}.npz --binSize 1000 --extendReads --centerReads
+plotCorrelation --corData {ChIP_target}.npz --corMethod spearman --whatToPlot heatmap --plotFile correlation_heatmap.pdf --skipZeros --plotFileFormat pdf --plotNumbers --plotTitle "ChIP_target_name"
+plotPCA --corData {ChIP_target}.npz --plotFile pca_plot.pdf --plotTitle "ChIP_target_name" --plotFileFormat pdf
 
 #deeptools coverage track
 bamCoverage -b {sample}_sorted.bam -o {sample}_coverage.bw --outFileFormat bigwig --binSize 10 --smoothLength 25 --extendReads --ignoreForNormalization chrX chrY BK000964.3 chrMT
 
 #deeptools normalised _coverage
-bamCompare -b1 {sample}_sorted.bam -b2 input_WT_rdna_merged.bam -o H3K9ac_WT_rep01_rdna.bw --scaleFactorsMethod None --operation ratio --binSize 10 --normalizeUsing RPKM --smoothLength 25 --extendReads --effectiveGenomeSize 45000
+bamCompare -b1 {sample}_sorted.bam -b2 {input}_merged.bam -o {sample}_ratio.bw --scaleFactorsMethod None --operation ratio --binSize 10 --normalizeUsing RPKM --smoothLength 25 --extendReads --ignoreForNormalization chrX chrY BK000964.3 chrMT
 
 #deeptools score matrix, heatmap and profile
-computeMatrix scale-regions -S {sample}.bw -R Mus_musculus.GRCm38.102.chr.gtf --regionBodyLength 2000 -b 5000 -a 5000 -o {sample}.mat.gz --skipZeros --missingDataAsZero --metagene
+computeMatrix scale-regions -S {sample}*.bw -R Mus_musculus.GRCm38.102.chr.gtf --regionBodyLength 2000 -b 5000 -a 5000 -o {sample}.mat.gz --skipZeros --missingDataAsZero --metagene
 plotHeatmap -m {sample}.mat.gz --dpi 300 -o {sample}.pdf
 
 #macs2 peak calling
-macs2 {sample}_sorted.bam -c {input_merged}.bam -g mm -n sample -q 0.01 --cutoff -f BAMPE --keep-dup all
+macs2 {sample}_sorted.bam -c {input}_merged.bam -g mm -n sample -q 0.01 --cutoff -f BAMPE --keep-dup all
+
+#Extract rDNA as a separate chromosome
+bamtools split -in {sample}_sorted.bam -reference
+
+#macs2 peak calling rDNA only 
+macs2 callpeak -t {sample}.REF_BK000964.3.bam -c {input}_merged.bam -n sample -g 4.5e4 -f BAMPE --outdir sample -q 0.01 --keep-dup all
 
